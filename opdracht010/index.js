ac8c77be780d49ee7693683ad14031cae88daec4
Roos61 <60176827+Roos61@users.noreply.github.com>
2021-06-08 11:19:51 +0200

    Add files via upload

---
+++ opdracht010/index.js
@@ -0,0 +1,15 @@
+console.log("Hello Winc Academy");
+let name = "Rosalie";
+let age = 59;
+console.log(name);
+console.log(age);
+console.log(4 + 4);
+console.log("4" + "4");
+console.log(100 - 101);
+console.log(100 * 8.5);
+console.log(17 % 4);
+typeof (age);
+typeof (name);
+let age = "59";
+typeof (age);
+
