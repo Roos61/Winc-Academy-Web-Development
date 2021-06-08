ee64fe4d8a30ce424750b5915ee5a060715ef90e
Roos61 <60176827+Roos61@users.noreply.github.com>
2021-06-08 16:15:08 +0200

    Add files via upload
    werken met JS via Node en Command Line, gebruik maken van comments

---
+++ opdracht012/script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc Academy studenten")
+//Bereken de uitkomst van 1230941 x 1823794, een grote som
+/*let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);*/
+//Bereken de uitkomst van 637263 / 54
+let division = 637263 / 54;
+console.log("division:", division);
