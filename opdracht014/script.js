71004f48cf11c456339316acd63e00231de9978b
Roos61 <60176827+Roos61@users.noreply.github.com>
2021-06-09 11:52:15 +0200

    Add files via upload
    Comparison and logical operators

---
+++ opdracht014/script.js
@@ -0,0 +1,45 @@
+const age = 29;
+const isFemale = false;
+const driverStatus = "bob";
+const name = "Sara";
+const totalAmount = 10;
+
+if (totalAmount > 25 && totalAmount <= 50) {
+    console.log("portie bitterballen on the house!")
+} else if (totalAmount > 50 && totalAmount < 100) {
+    console.log("portie nacho's on the house!")
+} else if (totalAmount >= 100) {
+    console.log("champagne on the house!")
+} else {
+    console.log("Hier is de rekening")
+}
+
+// if (age >= 18) {
+//     console.log("kom binnen!")
+// } else {
+//     console.log("sorry, geen toegang")
+// }
+
+// if (age >= 18 && age <= 25) {
+//     console.log("Je krijgt 50% korting!")
+// } else {
+//     console.log("Hier is de rekening")
+// }
+
+// if (name === "Bram" || name === "Sara") {
+//     console.log("Feestdag! Een gratis biertje")
+// } else {
+//     console.log("Hier is de rekening")
+// }
+
+// if (isFemale) {
+//     console.log("ladies night!!")
+// } else {
+//     console.log("alleen voor dames")
+// }
+
+// if (driverStatus === "bob") {
+//     console.log("je bent de bob, rij voorzichtig!")
+// } else {
+//     console.log("bel een taxi")
+// }
