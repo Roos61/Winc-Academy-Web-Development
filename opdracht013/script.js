431af941adda55a57162081cbf1c706962c11024
Roos61 <60176827+Roos61@users.noreply.github.com>
2021-06-09 10:42:34 +0200

    Add files via upload
    If- en else-if-statements

---
+++ opdracht013/script.js
@@ -0,0 +1,23 @@
+const age = 19;
+const isFemale = false;
+const driverStatus = "bob";
+
+if (age > 18) {
+    console.log("kom binnen!")
+} else if (age == 18) {
+    console.log("jij mag er ook in")
+} else {
+    console.log("sorry, geen toegang")
+}
+
+if (isFemale == true) {
+    console.log("ladies night!!")
+} else {
+    console.log("alleen voor dames")
+}
+
+if (driverStatus == "bob") {
+    console.log("je bent de bob, rij voorzichtig!")
+} else {
+    console.log("bel een taxi")
+}
