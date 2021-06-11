c39db2677312eafa7aec0a108cf8aa17a17c8ec0
Roos61 <60176827+Roos61@users.noreply.github.com>
2021-06-11 15:27:39 +0200

    Add files via upload
    muren schilderen in een function met 2 arguments

---
+++ opdracht017/script.js
@@ -0,0 +1,8 @@
+function paintWall(anyWall, colour) {
+    console.log("Painting the", anyWall, "wall " + colour);
+};
+//let tint = "red";
+let muur = "east";
+let tint = "green";
+
+paintWall(muur, tint);
